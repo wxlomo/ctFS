9ade0e01548b599433829ac7d7c9e7bbe8e9124a
Bruce <drhongjian@gmail.com>
2022-03-26 17:55:40 -0400

    Add to lock acquire

--- ctfs_sync.c
+++ ctfs_sync.c
@@ -21,7 +21,29 @@ inline void bit_lock_acquire(uint64_t *addr, uint64_t num){
 inline void bit_lock_release(uint64_t *addr, uint64_t num){
     FETCH_AND_unSET_BIT(addr, num);
 }
+//add a new node to the lock list upon the request
+void ctfs_lock_list_add_node(int fd, off_t start, size_t n, int flag) {
+    ct_fl_t* temp, * tail;
+    tail = ct_rt.file_range_lock[fd];   //get the head of the lock list
+    temp = (ct_fl_t*)malloc(sizeof(ct_fl_t));
+    temp->fl_next = NULL;
+    temp->fl_count = 1;
+    temp->fl_start = start;
+    temp->fl_end = start + n - 1;
+    temp->fl_flags = flag;
+    //TODO: get lock list here
+    while (tail != NULL) {
+        //check if current list contains a lock that is not compatable
+        if (check_overlap(tail, temp)) {
+            //overlap found stop the process
+            return -1;
+        }
+        tail = tail->fl_next;
+    }
+    tail->fl_next = temp;
+    //TODO: release locklist here
 
+}
 /************************************************ 
  * Implement file range lock
  ************************************************/
@@ -30,23 +52,24 @@ void ctfs_file_range_lock_init(){
 
 }
 
-/************************************************
-Inputs: fd, starting position, size, lock type
-Output: 0 if acquire the lock, error elsewise
-*************************************************/
+/**
+Acquire Lock, require to lock the 
+**/
 void ctfs_file_range_lock_acquire(int fd, off_t start, size_t n, int flag, ...){
     printf(fd);
-    // Range parameters already checked in pread/pwrite
-
-    switch (flag) {
-
-        case 0: // when it is requiring an read lock
-            while (ct_rt.file_range_lock[fd]->fl_next != NULL) {
-                
+    //TODO: Ensure atomic operation in list
+    ct_fl_t *tail;
+    tail = ct_rt.file_range_lock[fd];   //get the head of the lock list
+    
+    while (tail != NULL) {
+        if (((unsigned int)start <= tail.fl_end) && ((unsigned int)start + n >= tail.fl_start)) {
+            //If it is a write request, which does not tolerate range conflict, wait for the lock release
+            if (flag == 1) {
+                //TODO: potential spinlock
             }
-
-        case 1: // when it is requiring an write lock
-
+            // if there is conflict, but is in read mode
+            ctfs_lock_list_add_node(fd, start, n, flag);
+        }
     }
 
 
