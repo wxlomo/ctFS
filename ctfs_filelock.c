587f6e8d51057cf286476d2d35780c665cb33ffe
Wèixuān <weixuan.yang@mail.utoronto.ca>
2022-04-02 17:10:38 -0400

    Update ctfs_filelock.c

--- ctfs_filelock.c
+++ ctfs_filelock.c
@@ -161,7 +161,7 @@ ct_fl_t* ctfs_file_range_lock_acquire(int fd, off_t start, size_t n, int flag){
 
 ct_fl_t* ctfs_file_range_lock_try_acquire(int fd, off_t start, size_t n, int flag){
     ct_fl_t *temp = ctfs_lock_list_add_node(fd, start, n, flag);
-    if(temp->fl_block != NULL) return temp;
+    if(temp->fl_block == NULL) return temp;
     else return NULL;
 }
 
