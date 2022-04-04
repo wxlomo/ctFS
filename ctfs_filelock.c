4ff292e8ac174b70a0e208124034df338f074c7e
Wèixuān <weixuan.yang@mail.utoronto.ca>
2022-04-03 23:59:23 -0400

    Update ctfs_filelock.c

--- ctfs_filelock.c
+++ ctfs_filelock.c
@@ -64,9 +64,6 @@ inline void ctfs_lock_remove_blocking(ct_fl_t *current){
     while(temp != NULL){    // go through all node this is waiting for current node
         temp1 = temp->addr->fl_block;
         while(temp1 != NULL){   // go thorough the blocking list on other nodes to find itself
-            // compare range and mode
-            // if((temp1->addr->fl_start == current->fl_start) && (temp1->addr->fl_end == current->fl_end) && (temp1->addr->fl_type == current->fl_type)){
-            // or simply compares the address
             if(temp1->addr == current){
                 prev = temp1->prev;
                 next = temp1->next;
@@ -81,7 +78,9 @@ inline void ctfs_lock_remove_blocking(ct_fl_t *current){
             }
             temp1 = temp1->next;
         }
-        //printf("\tNode %p removed from Node %p blocking list\n", current, temp);
+#ifdef CTFS_DEBUG
+        printf("\tNode %p removed from Node %p blocking list\n", current, temp);
+#endif
         if(temp->next == NULL){ // last member in waiting list
             free(temp);
             current->fl_wait = NULL;
@@ -111,13 +110,16 @@ static inline ct_fl_t* ctfs_lock_list_add_node(int fd, off_t start, size_t n, in
 
     if(ct_fl.fl[fd] != NULL){
         tail = ct_fl.fl[fd];   // get the head of the lock list
-        while(tail != NULL){
-            // check if current list contains a lock that is not compatable
+        while(tail != NULL){ // check if current list contains a lock that is not compatable
             if(check_overlap(tail, temp) && check_access_conflict(tail, temp)){
                 ctfs_lock_add_blocking(temp, tail); //add the conflicted lock into blocking list
-                //printf("\tNode %p is blocking the Node %p\n", tail, temp);
+#ifdef CTFS_DEBUG
+                printf("\tNode %p is blocking the Node %p\n", tail, temp);
+#endif
                 ctfs_lock_add_waiting(temp, tail); //add the new node to the waiting list of the conflicted node
-                //printf("\tNode %p is waiting the Node %p\n", temp, tail);
+#ifdef CTFS_DEBUG
+                printf("\tNode %p is waiting the Node %p\n", temp, tail);
+#endif
             }
             last = tail;
             tail = tail->fl_next;
@@ -127,7 +129,9 @@ static inline ct_fl_t* ctfs_lock_list_add_node(int fd, off_t start, size_t n, in
     } else {
         ct_fl.fl[fd] = temp;
     }
-    //printf("Node %p added, Range: %u - %u, mode: %s\n", temp, temp->fl_start, temp->fl_end, enum_to_string(temp->fl_type));
+#ifdef CTFS_DEBUG
+    printf("Node %p added, Range: %u - %u, mode: %s\n", temp, temp->fl_start, temp->fl_end, enum_to_string(temp->fl_type));
+#endif
 
     seg_lock_release(&ct_fl.fl_lock[fd]);
 
@@ -136,7 +140,6 @@ static inline ct_fl_t* ctfs_lock_list_add_node(int fd, off_t start, size_t n, in
 
 /* remove a node from the lock list upon the request */
 static inline void ctfs_lock_list_remove_node(int fd, ct_fl_t *node){
-    assert(node != NULL);
     ct_fl_t *prev, *next;
 
     seg_lock_acquire(&ct_fl.fl_lock[fd]);
@@ -156,7 +159,9 @@ static inline void ctfs_lock_list_remove_node(int fd, ct_fl_t *node){
             next->fl_prev = prev;
     }
     ctfs_lock_remove_blocking(node);
-    //printf("Node %p removed, Range: %u - %u, mode: %s\n", node, node->fl_start, node->fl_end, enum_to_string(node->fl_type));
+#ifdef CTFS_DEBUG
+    printf("Node %p removed, Range: %u - %u, mode: %s\n", node, node->fl_start, node->fl_end, enum_to_string(node->fl_type));
+#endif
 
     seg_lock_release(&ct_fl.fl_lock[fd]);
 
@@ -187,5 +192,6 @@ ct_fl_t*  __attribute__((optimize("O0"))) ctfs_rlock_lock(int fd, off_t offset, 
 
 /* release the range lock */
 void ctfs_rlock_unlock(int fd, ct_fl_t *node){
+    assert(node != NULL);
     ctfs_lock_list_remove_node(fd, node);
 }
